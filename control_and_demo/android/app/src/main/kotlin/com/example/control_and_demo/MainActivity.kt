e03319c290e68b34b5532103c389b5c967185b9e
Topher05 <Tophermartus@gmail.com>
2025-07-01 12:58:39 -0400

    First Commit

---
+++ control_and_demo/android/app/src/main/kotlin/com/example/control_and_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.control_and_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ control_and_demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import gamepads_darwin
+import path_provider_foundation
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GamepadsDarwinPlugin.register(with: registry.registrar(forPlugin: "GamepadsDarwinPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
